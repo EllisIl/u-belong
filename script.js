36c2a867534cb82cc1afb5e648bd2037dd491781
Elijah Foard <efoard@byui.edu>
2024-11-05 13:23:32 -0700

    QoL

--- script.js
+++ script.js
@@ -1,31 +1,9 @@
 // Initialize the map and set the view to BYU-Idaho's coordinates
 const map = L.map("map", { scrollWheelZoom: false }).setView(
-  [43.8186, -111.7836],
+  [43.816, -111.783],
   16
 );
-// Hamburger menu functionality using the eye button
-let eye = document.getElementById("eye");
-let links = document.querySelector(".links");
-let sidebar = document.getElementById("sidebar");
-// Ensure the eye starts closed
-eye.classList.add("closed");
-links.classList.remove("open"); // Ensure the links are hidden initially
-sidebar.classList.remove("open"); // Ensure the sidebar is hidden initially
-// Toggle the eye opening/closing and the menu list visibility
-eye.addEventListener("click", () => {
-  // Toggle the eye open/close animation
-  if (eye.classList.contains("open")) {
-    eye.classList.remove("open");
-    eye.classList.add("closed");
-    links.classList.remove("open"); // Hide the menu when closing the eye
-    sidebar.classList.remove("open"); // Hide the sidebar when closing the eye
-  } else {
-    eye.classList.remove("closed");
-    eye.classList.add("open");
-    links.classList.add("open"); // Show the menu when opening the eye
-    sidebar.classList.add("open"); // Show the sidebar when opening the eye
-  }
-});
+
 // Add a tile layer (Map data from OpenStreetMap)
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "&copy; OpenStreetMap contributors",
@@ -72,6 +50,8 @@ async function highlightBuildings() {
 
     // Add event listeners
     addPolygonEventListeners(polygon);
+
+    return
   });
 }
 function addPolygonEventListeners(polygon) {
@@ -191,6 +171,7 @@ async function populateSidebar() {
   } catch (error) {
     console.error("Error fetching events:", error);
   }
+  return
 }
 let eventList = await fetchData('events.json');
 // Function to fetch events and save them to a file
